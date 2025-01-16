f72d69c929dc519a777a602e61000b2c161de48d
elfabri <navarro.fabricio.nahuel@gmail.com>
2025-01-16 16:49:16 -0300

    basics 4 compile

---
+++ 1_c-basics/4_compile/aaaa.c
@@ -0,0 +1,12 @@
+/*
+ * Run the code. Notice that the "starting..." message never prints.
+ * Fix the bug by removing the nonexistent function call.
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf("starting sneklang tool\n");
+    printf("finished sneklang tool\n");
+    return 0;
+}
