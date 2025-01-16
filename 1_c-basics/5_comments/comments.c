3ffd23440e1988e418283a13a9078111b8a7bba9
elfabri <navarro.fabricio.nahuel@gmail.com>
2025-01-16 16:53:19 -0300

    basics 5 comments

---
+++ 1_c-basics/5_comments/comments.c
@@ -0,0 +1,20 @@
+/*
+ * Looks like someone on the team doesn't know how to terminate a comment.
+ * Fix the bug.
+ * note: nvim btw
+ * */
+
+#include <stdio.h>
+
+int main() {
+    /*
+        Sneklang is for nvim enjoyers
+        who want to write their own garbage
+        collectors instead of using off-the-shelf
+        solutions
+    */
+    printf("i use sneklang btw\n");
+    printf("i use nvim btw\n");
+    printf("i use arch btw\n");
+    return 0;
+}
