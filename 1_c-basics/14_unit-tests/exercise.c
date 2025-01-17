0a9e7845301d04e03471050becd32116612ac649
elfabri <navarro.fabricio.nahuel@gmail.com>
2025-01-16 22:20:01 -0300

    basics 14 unit tests

---
+++ 1_c-basics/14_unit-tests/exercise.c
@@ -0,0 +1,10 @@
+/* Write the get_average (mathematical average) function
+ * in exercise.c based on the function prototype
+ * expected in exercise.h.
+ */
+
+#include "exercise.h"
+
+float get_average(int x, int y, int z) {
+    return (float) (x + y + z)/3;
+}
